63371fc86aae43067272875456aa84c9cfe5df73
yoni2k <yoni.k81@gmail.com>
2019-08-01 21:21:38 +0300

    Changing order of print for ease of comparison

--- src/optimizations.py
+++ src/optimizations.py
@@ -91,30 +91,30 @@ def optimize():
 
     for (features_left, outliers) in regs_dic:
         result = regs_dic[(features_left, outliers)]
-        if result['R2'] > max_r2:
-            max_r2 = result['R2']
-            max_r2_features = features_left
-            max_r2_outliers = outliers
-            max_r2_values = result
         if result['Diff mean'] + result['Diff STD'] < min_mean_plus_std:
             min_mean_plus_std = result['Diff mean'] + result['Diff STD']
             min_mean_plus_std_features = features_left
             min_mean_plus_std_outliers = outliers
             min_mean_plus_std_values = result
-        product = (1-result['R2']) * (result['Diff mean'] + result['Diff STD'])
+        if result['R2 Adj'] > max_r2:
+            max_r2 = result['R2 Adj']
+            max_r2_features = features_left
+            max_r2_outliers = outliers
+            max_r2_values = result
+        product = (1-result['R2 Adj']) * (result['Diff mean'] + result['Diff STD'])
         if product < min_product:
             min_product = product
             min_product_features = features_left
             min_product_outliers = outliers
             min_product_values = result
 
-    print(f"\nmax_R2: {max_r2}, features: {max_r2_features}, outliers:")
-    print(max_r2_outliers)
-    print(MyLinearRegression.get_main_results(max_r2_values))
-
     print(f"\nmin_mean_plus_std: {round(min_mean_plus_std, 2)}, features: {min_mean_plus_std_features}, outliers: ")
     print(min_mean_plus_std_outliers)
     print(MyLinearRegression.get_main_results(min_mean_plus_std_values))
+
+    print(f"\nmax_R2_adjusted: {max_r2}, features: {max_r2_features}, outliers:")
+    print(max_r2_outliers)
+    print(MyLinearRegression.get_main_results(max_r2_values))
 
     print(f"\nmin_product: {round(min_product,2)}, features: {min_product_features}, outliers:")
     print(min_product_outliers)
